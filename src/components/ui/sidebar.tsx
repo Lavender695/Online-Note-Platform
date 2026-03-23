d635f37ff58d4b77a59b0a46999f484dcb1d10f8
Lavender695 <2513690786@qq.com>
2026-03-23 14:18:34 +0800

    修复布局高度问题

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -248,7 +248,7 @@ function Sidebar({
       <div
         data-slot="sidebar-container"
         className={cn(
-          "fixed top-10 bottom-0 z-10 hidden h-[calc(100svh-2.5rem)] w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex",
+          "fixed top-0 bottom-0 z-10 hidden w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex",
           side === "left"
             ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
